6aebc6f949382b6e5e65c982e8067001934b0610
it-svit <dev@itsvit.org>
2018-03-05 11:40:43 +0200

    Fix enabled option

--- main.tf
+++ main.tf
@@ -10,7 +10,8 @@ module "label" {
 }
 
 resource "aws_iam_role" "lambda" {
-  name = "${module.label.id}"
+  count = "${var.enabled == "true" ? 1 : 0}"
+  name  = "${module.label.id}"
 
   assume_role_policy = <<EOF
 {
@@ -30,41 +31,42 @@ EOF
 }
 
 resource "aws_iam_role_policy" "lambda" {
-  name = "${module.label.id}"
-  role = "${aws_iam_role.lambda.name}"
+  count = "${var.enabled == "true" ? 1 : 0}"
+  name  = "${module.label.id}"
+  role  = "${aws_iam_role.lambda.name}"
 
   policy = "${var.iam_policy_document}"
 }
 
 resource "aws_lambda_function" "lambda" {
+  count            = "${var.enabled == "true" ? 1 : 0}"
   runtime          = "${var.runtime}"
   filename         = "${var.lambda_zipfile}"
   function_name    = "${module.label.id}"
   role             = "${aws_iam_role.lambda.arn}"
   handler          = "${var.handler}"
   source_code_hash = "${var.source_code_hash}"
-  count            = "${var.enabled}"
   timeout          = "${var.timeout}"
 }
 
 resource "aws_lambda_permission" "cloudwatch" {
+  count         = "${var.enabled == "true" ? 1 : 0}"
   statement_id  = "AllowExecutionFromCloudWatch"
   action        = "lambda:InvokeFunction"
   function_name = "${aws_lambda_function.lambda.arn}"
   principal     = "events.amazonaws.com"
   source_arn    = "${aws_cloudwatch_event_rule.lambda.arn}"
-  count         = "${var.enabled}"
 }
 
 resource "aws_cloudwatch_event_rule" "lambda" {
+  count               = "${var.enabled == "true" ? 1 : 0}"
   name                = "${module.label.id}"
   schedule_expression = "${var.schedule_expression}"
-  count               = "${var.enabled}"
 }
 
 resource "aws_cloudwatch_event_target" "lambda" {
+  count     = "${var.enabled == "true" ? 1 : 0}"
   target_id = "${module.label.id}"
   rule      = "${aws_cloudwatch_event_rule.lambda.name}"
   arn       = "${aws_lambda_function.lambda.arn}"
-  count     = "${var.enabled}"
 }
